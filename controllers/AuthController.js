c9946b46a3775b4c3739b254c0c1166bd2895eca
MamaWin01 <marcellinton0852@gmail.com>
2023-12-11 20:36:22 +0700

    Revert "version 4"
    This reverts commit 9cd24a268bccbc85359959903e57247c2a5f0720.

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,32 +1,32 @@
-import User from '../models/account.js';
-import jwt from 'jsonwebtoken';
-import { expressJwt } from 'express-jwt'; // Correct import statement
+import User from '../models/account.js'
+import jwt from 'jsonwebtoken'
+import { expressjwt } from 'express-jwt'
 
 // setup process.env
-import dotenv from 'dotenv';
+import dotenv from 'dotenv'
 dotenv.config();
 
-const jwtsecret = process.env.JWTSECRET;
+const jwtsecret = process.env.JWTSECRET
 
 const signin = async (req, res, next) => {
-  console.log(req.body);
-  let user = await User.findOne({'name': req.body.name});
+  console.log(req.body)
+  let user = await User.findOne({'name': req.body.name})
 
-  if (!user || !user.authenticate(req.body.password)) {
+  if(!user || !user.authenticate(req.body.password)){
     return res.status(401).json({
       error: 'Email or password not match'
-    });
+    })
   }
 
   const token = jwt.sign({
     _id: user._id
   }, jwtsecret, {
     algorithm: "HS256"
-  });
+  })
 
   res.cookie("t", token, {
     expire: new Date() + 9999
-  });
+  })
 
   return res.json({
     token,
@@ -35,24 +35,24 @@ const signin = async (req, res, next) => {
       name: user.name,
       email: user.email,
     }
-  });
-};
+  })
+}
 
 const signout = async (req, res, next) => {
-  res.clearCookie("t");
+  res.clearCookie("t")
   return res.status(200).json({
     message: "Signed out"
-  });
-};
+  })
+}
 
-const checkSignin = expressJwt({ // Correct method name
+const checkSignin = expressjwt({
   secret: jwtsecret,
   algorithms: ["HS256"],
   userProperty: 'auth'
-});
+})
 
 export default {
   signin,
   signout,
   checkSignin
-};
+}
