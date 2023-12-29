6e36240c4fae94438a2d8f1d3fe0efac4f6e8a80
MamaWin01 <marcellinton0852@gmail.com>
2023-12-29 18:12:29 +0700

    add edit account and delete account v2

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json({limit: "20mb"}));
+app.use(bodyParser.json({limit: "200mb"}));
 app.use(cors())
 
 app.use('/', userRoutes);
